2c5137d3960cc3408defe0cae65130cf7f976cb2
SushilAryal1234 <aryalsushil433@gmail.com>
2024-04-03 18:03:32 -0400

    Login Added

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/durpintm/user-management/initializers"
 	"github.com/durpintm/user-management/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -42,4 +45,49 @@ func SignUp(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully!"})
 }
 
-// Login
+func Login(c *gin.Context) {
+
+	// Get the email and password from the request body
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read the request body"})
+		return
+	}
+
+	// Look up for registered user
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	// Compare sent in password with the registered user password hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":   user.ID,
+		"email": user.Email,
+		"exp":   time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create token"})
+		return
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", true, true)
+	// Send back the JWT token
+	c.JSON(http.StatusOK, gin.H{})
+}
