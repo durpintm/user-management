2e2fbd355ed2e95c0c3ad8b20b7f17f41f7bfe01
virpal17 <virpalkaur170693@gmail.com>
2024-04-03 13:05:13 -0400

    LoadVariables_VirpalKaur

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -1,7 +1,15 @@
 package initializers
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func LoadEnvVariables() {
-	fmt.Println("Load Env variables")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
